e896b0af0efecb82f713241cdf031cc72e59a4a1
amurthy <am2275@cornell.edu>
2013-07-24 10:30:14 -0700

    Test commit

--- AmazingMaze/HelloScene.m
+++ AmazingMaze/HelloScene.m
@@ -49,6 +49,7 @@
     SKNode *helloNode = [self childNodeWithName:@"helloNode"];
     if(helloNode!=nil) {
         helloNode.name = nil;
+        
         SKAction *moveUp = [SKAction moveByX:0 y:100.0 duration:0.5];
         SKAction *zoom = [SKAction scaleTo:2.0 duration: 0.25];
         SKAction *pause = [SKAction waitForDuration: 0.5];
@@ -56,8 +57,10 @@
         SKAction *remove = [SKAction removeFromParent];
         SKAction *moveSequence = [SKAction sequence:@[moveUp,zoom,pause,fadeAway,remove]];
         
+        SKScene *spaceshipScene = [[SpaceshipScene alloc]initWithSize:self.size];
+        spaceshipScene.position = (CGPoint) {100,100};
+        
         [helloNode runAction: moveSequence completion:^{
-            SKScene *spaceshipScene = [[SpaceshipScene alloc]initWithSize:self.size];
             SKTransition *doors = [SKTransition doorsOpenVerticalWithDuration:0.5];
             [self.view presentScene:spaceshipScene transition:doors];
         }];
--- AmazingMaze/SpaceshipScene.m
+++ AmazingMaze/SpaceshipScene.m
@@ -7,7 +7,7 @@
 //
 
 #import "SpaceshipScene.h"
-
+#import <CoreMotion/CoreMotion.h>
 @interface SpaceshipScene()
 
 @property BOOL contentCreated;
@@ -27,20 +27,63 @@
     self.backgroundColor = [SKColor blackColor];
     self.scaleMode = SKSceneScaleModeAspectFit;
     SKSpriteNode *spaceship = [self newSpaceship];
-    spaceship.position = (CGPoint) {CGRectGetMidX(self.frame)};
+    spaceship.position = (CGPoint) {self.frame.size.width/2,self.frame.size.height/2};
+    
+    NSLog(@"%@", NSStringFromCGRect( self.frame));
+    SKSpriteNode *base = [[SKSpriteNode alloc] initWithColor:[SKColor blueColor] size:(CGSize) {2*self.frame.size.width,200}];
+    SKSpriteNode *top = [[SKSpriteNode alloc] initWithColor:[SKColor blueColor] size:(CGSize) {2*self.frame.size.width,200}];
+    
+    top.position = (CGPoint) {self.frame.size.width/2, self.frame.size.height};
+    top.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:top.size];
+    top.physicsBody.dynamic= NO;
+    
+    base.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:base.size];
+    base.physicsBody.dynamic= NO;
+    
+    
+    [self addChild:base];
+    [self addChild:top];
     [self addChild:spaceship];
 }
 
 -(SKSpriteNode*) newSpaceship {
     SKSpriteNode *hull = [[SKSpriteNode alloc] initWithColor:[SKColor grayColor] size:(CGSize){64,32}];
     
-    SKAction *hover = [SKAction sequence:@[
-                                          [SKAction waitForDuration:1.0],
-                                          [SKAction moveByX:100 y:50.0 duration:1.0],
-                                          [SKAction waitForDuration:1.0],
-                                          [SKAction moveByX:-100.0 y:-50 duration:1.0]]];
-    [hull runAction:[SKAction repeatActionForever:hover]];
+    hull.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:hull.size];
+    hull.physicsBody.dynamic = YES;
+    [hull.physicsBody setUsesPreciseCollisionDetection:YES];
+    
+    SKSpriteNode *light1 = [self newLight];
+    light1.position = (CGPoint) {-28.0,0.0};
+    [hull addChild:light1];
+    
+    SKSpriteNode *light2 = [self newLight];
+    light1.position = (CGPoint) {28.0, 6.0};
+    [hull addChild:light2];
+    
+    
+    
     return hull;
 }
+
+-(SKSpriteNode*) newLight {
+    SKSpriteNode *light = [[SKSpriteNode alloc] initWithColor:[SKColor yellowColor] size:(CGSize) {8,8}];
+    SKAction *blink = [SKAction sequence:@[
+                                          [SKAction fadeOutWithDuration:0.25],
+                                          [SKAction fadeInWithDuration:0.25]]];
+    SKAction *blinkForever = [SKAction repeatActionForever:blink];
+    
+    [light runAction:blinkForever];
+    return light;
+}
+
+-(NSMutableSet *) rectangleNodesWithHeight: (CGFloat) height andWidth: (CGFloat) width{
+    NSMutableSet *nodes = [[NSMutableSet alloc] init];
+    CGFloat thickness = 20.0;
+    SKSpriteNode *top = [[SKSpriteNode alloc] initWithColor:[SKColor redColor] size:(CGSize){width, thickness}];
+    SKSpriteNode *right = [[SKSpriteNode alloc] initWithColor:[SKColor redColor] size:(CGSize){thickness, height}];
+    return nodes;
+}
+
 
 @end
